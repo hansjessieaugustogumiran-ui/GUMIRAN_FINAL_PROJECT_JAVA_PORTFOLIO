cbed020c1868ccb8c144d6afa4e5e67717ce89ec
Hans Gumiran <hansjessieaugusto.gumiran@cvsu.edu.ph>
2026-01-18 23:26:06 +0800

    Create OrderStatus.java

---
+++ LABORATORY ACTIVITY 7/OrderStatus.java
@@ -0,0 +1,13 @@
+package com.store.order;
+
+public enum OrderStatus {
+    PENDING,
+    PAID,
+    SHIPPED,
+    CANCELLED
+}
+
+// GUMIRAN, HANS JESSIE AUGUSTO M.
+// BSCS-2D
+// AI USED: CHATGPT
+// LINK CONVO: https://chatgpt.com/share/696cf453-e8b0-8005-8304-112cef24edbd
